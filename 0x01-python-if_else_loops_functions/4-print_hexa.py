75e8f969b610d321c6f030bf4156814927dadb9c
Chebe97 <joechebe@gmail.com>
2021-11-16 20:17:36 +0000

    a few adjustments

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,5 +2,5 @@
 #A program that prints all numbers 0 yo 98
 #In decimal and hexadecimal
 
-for n in range(0, 99):
-    print(n, "=", hex(n))
+for number in range(0, 99):
+    print(number, "=", hex(number))
