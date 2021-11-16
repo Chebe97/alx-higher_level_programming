dec121c4b5dce289b235b3bfe7c3e57a897af678
Chebe97 <joechebe@gmail.com>
2021-11-16 22:14:58 +0000

    New additions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,7 @@
 #A program that prints number 0 to 99
 #Numbers must be followed by a , and a space
 for number in range(0, 100):
-    print("{0:02d}".format(number), end = ", ")
-#else:
- #   print(number)
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{0:02d}".format(number), end = ", ")
