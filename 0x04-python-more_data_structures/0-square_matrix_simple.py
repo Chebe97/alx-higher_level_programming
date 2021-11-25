03de36f36de6f3585224297037d1c0f84f9deebb
Chebe97 <joechebe@gmail.com>
2021-11-25 21:51:34 +0000

    New Changes

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,5 @@ def square_matrix_simple(matrix=[]):
     row_index = 0
     for row in matrix:
         new_matrix[row_index] = [x**2 for x in row]
-        row_index +=1
-    return new_matrix
-
-    
+        row_index += 1
+    return(new_matrix)
