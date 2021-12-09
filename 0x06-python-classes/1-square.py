9d583c35d14af0a4c734f7834899064d0e5e0c57
Chebe97 <joechebe@gmail.com>
2021-12-09 12:55:41 +0000

    New comms

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A class that defines a square"""
+
+
+class Square:
+    """size of a square"""
+
+    def __init__(self, size):
+        """New square initialization
+
+        Args:
+            size (int): the sie of the square
+        """
+        self.__size = size
+    
