3dcb7f3fc5015e62cbc1f28e13f11822b6327003
Chebe97 <joechebe@gmail.com>
2021-12-09 18:26:54 +0000

    Commit

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,21 +1,21 @@
-#!/usr/bin/pyhton3
-"""A module that creates a class Square"""
+#!/usr/bin/python3
+"""Define a class Square."""
 
 
 class Square:
-    """Defining a square"""
-    def __init__(self, size=0):
-        """Initialize a new square
+    """Represent a square."""
 
+    def __init__(self, size=0):
+        """Initialize a new square.
         Args:
-            size (int)
+            size (int): The size of the new square.
         """
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
 
-        def area(self):
-            """Return the area of the square"""
-            return self.__size * self.__size
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
